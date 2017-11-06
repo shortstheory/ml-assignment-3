185c2e74911fd63b8360c5b57aa240e5362775cb
arnav dhamija <arnav.dhamija@gmail.com>
2017-11-07 01:16:50 +0530

    Man i tired

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -10,6 +10,8 @@ import java.sql.*;
 
 public class DatabaseHelper {
     final private static String testFolderPath = "/home/nic/original-projects/ml3/aclImdb/train/";
+    final public static String positiveString = "pos";
+    final public static String negativeString = "neg";
 
     private void listAllFiles(String classification) {
         File folder = new File(testFolderPath + classification);
@@ -26,16 +28,33 @@ public class DatabaseHelper {
             statement = connection.createStatement();
             String str = FileUtils.readFileToString(file);
             String processedString = str.replaceAll("[^A-Za-z0-9']", " ");
+            processedString = processedString.replaceAll("'", "");
             String[] words = processedString.split("\\s+");
             for (int i = 0; i < words.length; i++) {
                 String lowerCaseWord = words[i].toLowerCase();
-                String sql = "insert into WordsTable values ('" + lowerCaseWord + "', '" + classification + "')";
+//                String sql = "insert into WordsTable values ('" + "Rahim" + "','" + "Good" + "')";
+                String sql = "insert into WordsTable values ('" + lowerCaseWord + "','" + classification + "')";
+//                String sql = "insert into WordsTable values ('GoodBoy', 'Pos')";
                 statement.executeUpdate(sql);
 //                System.out.println(words[i]);
             }
             statement.close();
         } catch (IOException e) {
             System.out.println("File not exists");
+        } catch (SQLException se) {
+            System.out.println("SQL problem!" + se.getMessage());
+        }
+    }
+
+    public void getCount(String word, String classification) {
+        try {
+            statement = connection.createStatement();
+            String sql = "select count(*) as rowcount from WordsTable where word = '" + word + "' and classification = '" + classification + "'";
+            ResultSet rs = statement.executeQuery(sql);
+            rs.next();
+            int count = rs.getInt("rowcount");
+            System.out.println(count);
+
         } catch (SQLException se) {
             System.out.println("SQL problem!" + se.getMessage());
         }
@@ -48,7 +67,7 @@ public class DatabaseHelper {
     DatabaseHelper() {
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
             String sql;
             statement = connection.createStatement();
             sql = "create table WordsTable (word text, classification text)";
@@ -62,7 +81,17 @@ public class DatabaseHelper {
 
     public static void main(String[] args) {
         DatabaseHelper databaseHelper = new DatabaseHelper();
-        databaseHelper.listAllFiles("neg/");
-        System.out.println("Good");
+        databaseHelper.listAllFiles("neg");
+        databaseHelper.listAllFiles("pos");
+        databaseHelper.getCount("terrible", "neg");
+        databaseHelper.getCount("terrible", "pos");
+        databaseHelper.getCount("fuck", "neg");
+        databaseHelper.getCount("fuck", "pos");
+        try {
+            System.out.println("Good");
+            databaseHelper.connection.close();
+        } catch (SQLException se) {
+
+        }
     }
 }
