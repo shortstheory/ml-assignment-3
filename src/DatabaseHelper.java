2889009daffd65c83fee04a81b1fa68bad4233bb
arnav dhamija <arnav.dhamija@gmail.com>
2017-11-07 00:01:42 +0530

    updated

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -1,11 +1,28 @@
 import javax.xml.transform.Result;
+import java.io.File;
 import java.sql.*;
 
 public class DatabaseHelper {
+    final private static String testFolderPath = "/home/nic/original-projects/ml3/aclImdb/test/";
+
+    private static void listAllFiles(String classification) {
+        File folder = new File(testFolderPath + classification);
+        File[] listOfFiles = folder.listFiles();
+        System.out.println(listOfFiles.length);
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
-
+        listAllFiles("neg/");
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite::memory:");
