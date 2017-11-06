e4243e037f6d2f99b6de6f13f28e22a0e7f09f36
arnav dhamija <arnav.dhamija@gmail.com>
2017-11-07 00:39:40 +0530

    Work

--- src/DatabaseHelper.java
+++ src/DatabaseHelper.java
@@ -1,28 +1,51 @@
+import org.apache.commons.io.FileUtils;
+
 import javax.xml.transform.Result;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class DatabaseHelper {
-    final private static String testFolderPath = "/home/nic/original-projects/ml3/aclImdb/test/";
+    final private static String testFolderPath = "/home/nic/original-projects/ml3/aclImdb/train/";
 
-    private static void listAllFiles(String classification) {
+    private void listAllFiles(String classification) {
         File folder = new File(testFolderPath + classification);
         File[] listOfFiles = folder.listFiles();
         System.out.println(listOfFiles.length);
 
         for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
+            processFile(listOfFiles[i], classification);
         }
     }
 
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        listAllFiles("neg/");
+    private void processFile(File file, String classification) {
+        try {
+            statement = connection.createStatement();
+            String str = FileUtils.readFileToString(file);
+            String processedString = str.replaceAll("[^A-Za-z0-9']", " ");
+            String[] words = processedString.split("\\s+");
+            for (int i = 0; i < words.length; i++) {
+                String lowerCaseWord = words[i].toLowerCase();
+                String sql = "insert into WordsTable values ('" + lowerCaseWord + "', '" + classification + "')";
+                statement.executeUpdate(sql);
+//                System.out.println(words[i]);
+            }
+            statement.close();
+        } catch (IOException e) {
+            System.out.println("File not exists");
+        } catch (SQLException se) {
+            System.out.println("SQL problem!" + se.getMessage());
+        }
+    }
+
+
+    Connection connection = null;
+    Statement statement = null;
+
+    DatabaseHelper() {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite::memory:");
@@ -30,27 +53,16 @@ public class DatabaseHelper {
             statement = connection.createStatement();
             sql = "create table WordsTable (word text, classification text)";
             statement.executeUpdate(sql);
-
-            sql = "insert into WordsTable values ('oh well', '+')";
-            statement.executeUpdate(sql);
-            sql = "insert into WordsTable values ('oh dear', '-')";
-            statement.executeUpdate(sql);
-
-            ResultSet rs = statement.executeQuery("select * from WordsTable");
-
-            while (rs.next()) {
-                String word = rs.getString("word");
-                String classification = rs.getString("classification");
-
-                System.out.println(word);
-                System.out.println(classification);
-            }
-
             statement.close();
-            connection.close();
+//            connection.close();
         } catch (Exception e) {
             System.out.println("Ohdear" + e.getClass().getName() + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        DatabaseHelper databaseHelper = new DatabaseHelper();
+        databaseHelper.listAllFiles("neg/");
         System.out.println("Good");
     }
 }
